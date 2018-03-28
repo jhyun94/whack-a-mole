1b263dc4030c3d6bdc4861998048393ce36bb019
jhyun94 <hyunjonathan@gmail.com>
2018-03-27 22:47:09 -0500

    field background

---
+++ src/components/field.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import fieldBG from '../images/grass.jpg';
+
+class Field extends Component {
+
+	render(){
+		return (
+			<div className="field-bg">
+				
+			</div>
+		)
+	}
+}
+
+export default Field;
