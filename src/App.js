873a1c78b162fd9ccbe8a8d8acef558266eda636
jhyun94 <hyunjonathan@gmail.com>
2018-03-31 18:44:13 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Field from './components/field';
+
 import Hole from './components/hole';
 import Score from './components/score';
 import PlayButton from './components/play-button';
